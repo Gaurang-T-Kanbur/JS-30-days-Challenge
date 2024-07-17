9c40ee066b0e2e52b3b29f526869252c40849b5b
Gaurang Kanbur <gaurangkanbur@gmail.com>
2024-07-17 23:32:31 +0530

    Day 4 done

--- Day_5/Day_5.js
+++ Day_5/Day_5.js
@@ -32,3 +32,42 @@ console.log(conca("abc","bcd"));
 //Task_5: Arrow function tocalculate sum
 const sumTwo = (a,b) => a + b;
 console.log(sumTwo(15,12));
+
+//Task_6: String contains character or not
+
+const charString = (str, char)=>{
+    if(str.includes(char)){
+        return true;
+    } else{
+        return false;
+    }
+
+}
+console.log(charString("Hello World", 'o'));
+
+//Task_7: Multiply with a default value
+function multiply(a, b=1){
+    return a*b;
+}
+console.log(multiply(9,9));
+
+//Task_8: Greeting Message
+function greeting(name, age = 18){
+    let message = `Hi, ${name}! Congratulations, You're ${age} years old`;
+    return message;
+
+}
+console.log(greeting("GTK"));
+
+//Task_9:Higher Order Function, Function that has function as its arguments and is passed as parameter to that function
+
+function sayHello(func, num){//Higher Order Function
+    for (let i = 0; i < num; i++) {
+       func(i);
+        
+    }
+}
+function Hello(i){
+    console.log ("Hello "+ i);
+}
+sayHello(Hello, 5);//Here reference is passed and it is executed '()' in the higher order function above
