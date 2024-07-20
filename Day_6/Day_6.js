a61019a63e3d13edd662d2d0e5ac942ac8973151
Gaurang Kanbur <gaurangkanbur@gmail.com>
2024-07-20 12:43:35 +0530

    Added Day_6

---
+++ Day_6/Day_6.js
@@ -0,0 +1,24 @@
+//Task_1: Create array from 1 to 5;
+let arr = [1,2,3,4,5];
+console.log(arr);
+
+//Task_2: First and last value of array
+console.log(arr[0],arr[arr.length-1]);
+
+//Task_3: Push in array
+arr.push(12);
+console.log(arr);
+//Task_4: Pop from array
+arr.pop();
+console.log(arr);
+console.log(arr.pop());
+//Task_5: Shift operation on array
+arr.shift();
+console.log(arr);
+
+//Task_6: Unshift Operation on array
+arr.unshift(9,9);
+console.log(arr.unshift(9,9));//returns the size of array
+console.log(arr);
+
+//Task_7: Map
