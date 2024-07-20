bae38b93f7c27eedbaaa6b561636d8f11a88bd28
Gaurang_T_Kanbur <gaurangkanbur@gmail.com>
2024-07-21 00:37:01 +0530

    Day 7

---
+++ Day_7/Day_7.js
@@ -0,0 +1,10 @@
+//Task_1: Object Creation Of book
+let book = {title:"Pralay:The Deluge",
+    author:"Vineet Bajpai",
+    year:"2020"
+}
+console.log(book);
+
+//Task_2:Access Properties
+console.log(book.title);
+console.log(book.author);
